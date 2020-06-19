31181c572636606fda42122cece905375ec461e5
Daniel Müller <daniel.mueller@docufy.de>
2020-06-19 15:15:20 +0200

    Init

---
+++ src/NetCoreConsoleClient/Configuration/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreConsoleClient
+{
+    public class Config
+    {
+        public string ClientId { get; set; }
+        public string IssuerUrl { get; set; }
+        public string Scope { get; set; }
+    }
+}
---
+++ src/NetCoreConsoleClient/Configuration/ConfigLoader.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel.OidcClient;
+using System;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace NetCoreConsoleClient.Configuration
+{
+    public class ConfigLoader
+    {
+        private readonly string configfileFullPath;
+        private readonly string directoryPath;
+        private readonly string fileName;
+
+        public ConfigLoader(string fileName)
+        {
+            this.fileName = fileName;
+            directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+            configfileFullPath = Path.Combine(directoryPath, fileName);
+        }
+
+        public OidcClientOptions LoadConfigFromFile()
+        {
+            var input = new StreamReader(configfileFullPath);
+            var deserializer = new Deserializer();
+            var config = deserializer.Deserialize<Config>(input);
+
+            var oidcOptions = new OidcClientOptions();
+
+            if (config != null)
+            {
+                oidcOptions.Authority = config.IssuerUrl;
+                oidcOptions.Scope = config.Scope;
+                oidcOptions.ClientId = config.ClientId;
+                oidcOptions.FilterClaims = false;
+                oidcOptions.Policy.RequireAccessTokenHash = false;
+                oidcOptions.Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode;
+                oidcOptions.ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect;
+            }
+
+            return oidcOptions;
+        }
+    }
+}
